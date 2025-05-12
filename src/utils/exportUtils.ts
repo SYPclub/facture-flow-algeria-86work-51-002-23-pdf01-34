0abaff1267ddfa441346e6de46e941e5edcd8247
jhfpasldo <bilalkhamri3@gmail.com>
2025-05-12 16:45:03 +0100

    join

--- src/utils/exportUtils.ts
+++ src/utils/exportUtils.ts
@@ -79,18 +79,25 @@ export const exportProformaInvoiceToPDF = async (proforma: ProformaInvoice) => {
   pdf.text('Billed To:', 14, clientY);
   pdf.setFontSize(10);
   pdf.text([
+    [
     `client: ${proforma.client?.name || ''}`,
     `NIF: ${proforma.client?.taxid || ''}`,
     proforma.client?.nis && `NIS: ${proforma.client.nis}`,
+    ].filter(Boolean).join(' | '),
+    [
     proforma.client?.rc && `rc: ${proforma.client.rc}`,
     proforma.client?.ai && `ai: ${proforma.client.ai}`,
     proforma.client?.ccp && `ccp: ${proforma.client.ccp}`,
+    ].filter(Boolean).join(' | '),
+    [
     proforma.client?.rib && `rib: ${proforma.client.rib}`,
     proforma.client?.contact && `contact: ${proforma.client.contact}`,
     proforma.client?.telcontact && `telcontact: ${proforma.client.telcontact}`,
-    `Address: ${proforma.client?.address || ''}`,
-    `ville: ${proforma.client?.city || ''}, ${proforma.client?.country || ''}`,
+    ].filter(Boolean).join(' | '),
+    
+    `Address: ${proforma.client?.address || ''} | ville: ${proforma.client?.city || ''}`,
     `Telephone: ${proforma.client?.phone || ''} , email: ${proforma.client?.email || ''} `
+    
   ].filter(Boolean), 14, clientY + 12);
   
   pdf.setFontSize(10);
