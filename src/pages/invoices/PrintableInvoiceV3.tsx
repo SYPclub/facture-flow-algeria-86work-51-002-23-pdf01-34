b539c7a51779cf35ee2af0b5d1d397df907bb12a
Startup program for youth (SPY) <khomri12@gmail.com>
2025-05-23 12:19:17 +0100

    import CompanyInfoHeader from "@/components/exports/CompanyInfoHeader";

--- src/pages/invoices/PrintableInvoiceV3.tsx
+++ src/pages/invoices/PrintableInvoiceV3.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { supabase } from "@/integrations/supabase/client";
-import { CompanyInfoHeader } from "@/components/exports/CompanyInfoHeader";
+import CompanyInfoHeader from "@/components/exports/CompanyInfoHeader";
 import { useCompanyInfo } from "@/hooks/useCompanyInfo";
 
 interface InvoiceItem {
