711457d923b24e23e32df9ed3fdb66abdbe8a94b
Startup program for youth (SPY) <khomri12@gmail.com>
2025-05-25 12:54:54 +0100

    PNG

--- src/utils/exportUtils.ts
+++ src/utils/exportUtils.ts
@@ -74,7 +74,7 @@ const addHeader = async (pdf: jsPDF, documentType: string, documentNumber: strin
   if (logoBase64) {
       try {
         // Add logo to the left of the company name
-        pdf.addImage(logoBase64, 'PNG', 14, 10, 30, 30); // Adjust dimensions as needed
+        pdf.addImage(logoBase64, 'PNG', 350, 10, 30, 30); // Adjust dimensions as needed
       } catch (error) {
         console.error('Error adding logo:', error);
       }
